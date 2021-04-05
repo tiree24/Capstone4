85e4777d2e402a11d40fc29ce6857284d326f71c
Amanda Simmons <carpe737diem@gmail.com>
2021-04-05 15:37:22 -0400

    added .env file to .gitignore (moade secret key secre)

--- capstone/settings.py
+++ capstone/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wg1k96r#c__%!nn(##kj&(&i67p(yw$m4v@nlf#d-#g_#j@&_z'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
