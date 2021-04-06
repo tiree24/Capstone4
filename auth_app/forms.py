45bcd9ba34474f3624b319f6bb2dcac1f1945f4b
Tiree Jackson <tiree_jackson@yahoo.com>
2021-04-06 14:01:02 -0400

    i forgot to comment out my forms going to use amanda form

--- auth_app/forms.py
+++ auth_app/forms.py
@@ -1,8 +1,8 @@
-from django import forms
+# from django import forms
 
 
 
 
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=150)
-    password = forms.CharField(widget=forms.PasswordInput)
+# class LoginForm(forms.Form):
+#     username = forms.CharField(max_length=150)
+#     password = forms.CharField(widget=forms.PasswordInput)
