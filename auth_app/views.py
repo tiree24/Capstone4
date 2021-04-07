a4d6ca0626433ce132763d30914d9ddbca8726f7
Ian Waddell <i.83.waddell@gmail.com>
2021-04-06 19:56:30 -0500

    Quick fixes

--- auth_app/views.py
+++ auth_app/views.py
@@ -9,7 +9,7 @@ class LoginFormView(View):
 
     def get(self, request):
         form = LoginForm()
-        return render(request, "generic_form.html", { 'form': form})
+        return render(request, "generic_form.html", { 'form': form, 'heading': 'Login below'})
 
     def post(self, request):
         form = LoginForm(request.POST)
