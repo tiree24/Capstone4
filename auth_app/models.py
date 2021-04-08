36b13c7c197d642ad642f4911d1b4808cd59547d
Amanda Simmons <carpe737diem@gmail.com>
2021-04-08 15:45:54 -0400

    favoriting functionality works in the admin panel

--- auth_app/models.py
+++ auth_app/models.py
@@ -5,7 +5,7 @@ from backend.models import FileUpload
 class MyCustomUser(AbstractUser):
     email = models.EmailField()
     password = models.CharField(max_length=20)
-    favorite = models.ManyToManyField(FileUpload, related_name='user_favorites', blank=True)
+    favorites = models.ManyToManyField(FileUpload, related_name='user_favorites', blank=True)
 
     def __str__(self):
         return self.username
--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.utils.timezone import now
 
-class FileUpload(models.Model):
+class FileUpload(models.Model):  
     upload = models.FileField()
     date_time = models.DateTimeField(default=now)
     search_str = models.TextField(max_length=50)
+    favorite = models.BooleanField(default=False)
 
     def __str__(self):
         return self.search_str
