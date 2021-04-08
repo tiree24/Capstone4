075309ec0baf044fd393ac00df3a314776f825d3
Ian Waddell <i.83.waddell@gmail.com>
2021-04-08 13:50:20 -0500

    bug fix

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from backend.models import FileUpload
 
 class MyCustomUser(AbstractUser):
     email = models.EmailField()
     password = models.CharField(max_length=20)
-    favorite = models.ManyToManyField("FileUpload", related_name='user_favorites', blank=True)
+    favorite = models.ManyToManyField(FileUpload, related_name='user_favorites', blank=True)
 
     def __str__(self):
         return self.username
