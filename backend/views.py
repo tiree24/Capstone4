bfb360f54dab0340642aec5ceed6f45fff3632b9
Melissa Gutierrez <melgutierrez.1193@gmail.com>
2021-04-13 11:22:15 -0500

    delete_works

--- backend/views.py
+++ backend/views.py
@@ -44,15 +44,18 @@ class UploadView(View):
 
 
 def file_list(request):
+    test = FileUpload.objects.all()
     files = FileUpload.objects.all().order_by('-date_time')
+    
     recent = FileUpload.objects.all().order_by('-date_time')[:3]
     # breakpoint()
+ 
+    
     return render(request, 'file_list.html', {"files": files, "recent": recent})
 
 def delete_file(request, pk):
     if request.method == "POST":
         files = FileUpload.objects.get(pk=pk)
-        # breakpoint()
         files.delete()
         full_path = os.path.join(settings.MEDIA_ROOT, files.upload.path)
         os.unlink(full_path)
@@ -62,6 +65,7 @@ def delete_file(request, pk):
    
 
 
-# deleting instance from db
-# delete file form sys
+# deleting instance from db WORKS!!
+# delete file form sys WORKS!!
+# FILE_LIST NOT GRABBING THE CORRECT ATTRIBUTES FROM THE MODEL
 
