83c38ed3cd30b52108da07b4e3780e3e1346e70f
Ian Waddell <i.83.waddell@gmail.com>
2021-04-15 13:36:49 -0500

    Set debug to true

--- capstone/settings.py
+++ capstone/settings.py
@@ -27,7 +27,7 @@ CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
