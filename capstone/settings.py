1a98b81a696c4f4e136f69a203d1e1f8b8553486
Ian Waddell <i.83.waddell@gmail.com>
2021-04-15 13:13:08 -0500

    Adding Argon bootstrap

--- capstone/settings.py
+++ capstone/settings.py
@@ -17,6 +17,7 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -42,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'auth_app',
     'backend',
-    'crispy_forms',
 ]
 
 
@@ -128,7 +128,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(CORE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(CORE_DIR, 'capstone/static'),
+)
 
 AUTH_USER_MODEL = 'auth_app.MyCustomUser'
 
